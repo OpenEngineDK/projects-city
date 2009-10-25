bc42e333400b79e90cfe192ac20d623e75e1fb61
ptx <ptx@openengine.dk>
2009-10-25 15:18:01 +0000

    changed add-to-process

--- data/oe-init.scm
+++ data/oe-init.scm
@@ -1,21 +1,32 @@
 (define funs '())
 
-(define add-to-process (lambda (fn)
-	(set! funs (cons fn funs))
-))
+(define add-to-process (lambda (name fn)	
+	(set! funs 
+	(if (assoc name funs)
+		(map (lambda (x) 
+			(if (equal? name (car x))
+				(cons name fn)
+				x)
+			) funs)
+		(acons name fn funs)))
+		(display funs)
+		))
+		
+
+
 
 (define apply-process (lambda (n)
 	(map (lambda (fn)
-			(apply fn n '())
+			(apply (cdr fn) n '())
 		
 		) funs)))
 	
 	
 
 (define process (lambda (n)
-	(display n)
-	(display "\n")
-		;(apply-process n)
-		(test n)
+	;(display n)
+	;(display "\n")
+		(apply-process n)
+			;(test n)
 	))
 
--- data/test.scm
+++ data/test.scm
@@ -1,16 +1,22 @@
+(define dt 1)
+
 (define test 
-		(lambda (n)
-			;(vec-to-list (tn-get-pos dot-tn))
-			;(display 'test)
-			(let* (	(v (vec-to-list (oe-call dot-tn 'get-position)))
-				  	(x (car v))
-					(y (cadr v))
-					(z (caddr v))
-					(n-vec `(,x ,y ,(+ z 1))))
-				(oe-call dot-tn 'set-position (list-to-vec n-vec))
+	(lambda (n)
+		;(vec-to-list (tn-get-pos dot-tn))
+		;(display 'test)
+		(set! dt (+ dt (/ n 5000000)))
+		(let* (	(v (vec-to-list (oe-call dot-tn 'get-position)))
+			  	(x (car v))
+				(y (cadr v))
+				(z (caddr v))
+					
+			(n-vec `(,(+ (* (sin dt) 200) 400)
+					,y 
+					,(+ (* (cos dt) 200) 400))))
+		(oe-call dot-tn 'set-position (list-to-vec n-vec))
+		
+		;(display dt)
+		)))
+	
 			
-				(display n-vec)
-			)	
-			))
-			
-(add-to-process test)
+(add-to-process 'test test)
